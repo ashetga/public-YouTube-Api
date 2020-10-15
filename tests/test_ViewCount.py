1db5c70d24db3356ca2f129b2141fc6572448fb7
Mac Pro <macpro@Macs-Mac-Pro.home>
2020-10-14 22:05:17 -0700

    Testing GitHub actions w/ common operations

--- tests/test_ViewCount.py
+++ tests/test_ViewCount.py
@@ -1,8 +1,8 @@
 from app import getViewCount
-import secrets
+import test_secrets
 
 # validate the incoming viewCounts
 def test_ViewCount():
-    apiKey = secrets.apiKey
-    viewCount = getViewCount(apiKey, secrets.userName)
+    apiKey = test_secrets.apiKey
+    viewCount = getViewCount(apiKey, test_secrets.userName)
     assert int(viewCount) > 100000
