240203c8973e08d67bbadb20ae64bd382e4211ca
Mac Pro <macpro@Macs-Mac-Pro.home>
2020-10-14 23:14:03 -0700

    configure black on vscode

--- src/app.py
+++ src/app.py
@@ -1,12 +1,10 @@
-
-'''
+"""
 A simple app to test the functionality of google's Youtube Data Api
 
 We will be retreiving some public channel's statistics and compute some fun KPIs. 
    - Top 5 high viewership videos in a channel
    - Compute total duration of the entire channel
-'''
-
+"""
 
 
 from googleapiclient.discovery import build
@@ -14,29 +12,24 @@ from pprint import pprint
 import secrets
 
 
-
 def getViewCount(apiKey, userName):
 
     # build a service
-    yT = build('youtube', 'v3', developerKey=apiKey)
+    yT = build("youtube", "v3", developerKey=apiKey)
 
     # generate Stats request for a given channel
-    request = yT.channels().list(
-        part = 'statistics',
-        forUsername = userName
-    )
+    request = yT.channels().list(part="statistics", forUsername=userName)
 
     # collect stats
     response = request.execute()
 
     # extract viewCount
-    viewCount = response['items'][0]['statistics']['viewCount']
+    viewCount = response["items"][0]["statistics"]["viewCount"]
     return viewCount
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     apiKey = secrets.apiKey
     viewCount = getViewCount(apiKey, secrets.userName)
-    
-    print(viewCount)
 
+    print(viewCount)
