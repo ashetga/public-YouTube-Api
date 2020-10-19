795d7cddc28b9aac0a867ab9b204c4088a112e61
ashetga <60746259+ashetga@users.noreply.github.com>
2020-10-19 11:53:16 -0400

    Update test_ViewCount.py

--- tests/test_ViewCount.py
+++ tests/test_ViewCount.py
@@ -5,9 +5,14 @@ import secrets
 
 
 def test_ViewCount(apiKey):
+    
+    if apiKey:
+        print "Found api key from parameter"
     if not apiKey:
         apiKey = secrets.apiKey
+        print "Getting api key from secrets"
 
     playListId = "PL-osiE80TeTsWmV9i9c58mdDCSskIFdDS"
     viewCount = getViewCount(playListId)
+    print 'viewCount: {0}'.format(viewCount)
     assert int(viewCount) != 0
