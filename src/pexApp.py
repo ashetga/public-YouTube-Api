d4523bce41ea3dbd2c242b5fc3870b26138e5b4e
Mac Pro <macpro@Macs-Mac-Pro.local>
2020-10-19 09:01:31 -0700

    implemented run time secrets file generation

---
+++ src/pexApp.py
@@ -0,0 +1,28 @@
+import requests
+from pprint import pprint
+import json
+import secrets
+
+
+def getTakeDownPct(authToken, header, url):
+    assets = requests.get(url=url, headers=header).json()
+
+    takeDowns = 0
+    totalAssets = len(assets)
+
+    for asset in assets:
+        takeDowns += 1 if asset["is_taken_down"] == True else takeDowns
+
+    return takeDowns // totalAssets
+
+
+def run():
+    authToken = secrets.authToken
+    header = {"Authorization": f"Bearer {authToken}"}
+    url = secrets.url
+
+    pprint(getTakeDownPct(authToken, header, url))
+
+
+if __name__ == "__main__":
+    run()
