c94eec35260320bdfcdfd748373b284a43f6dcc5
ashetga <60746259+ashetga@users.noreply.github.com>
2020-10-19 15:24:37 -0400

    Update test_Print.py

--- tests/test_Print.py
+++ tests/test_Print.py
@@ -3,5 +3,5 @@ import sys
 print('Hello World!')
 
 print(f'Number of arguments: {len(sys.argv)}, arguments.')
-print(f'Argument List: {str(sys.argv)}')
+print(f'Argument List: {str(sys.argv)} {len(sys.argv[0])} {len(sys.argv[2])}')
 print(f'Matches? {sys.argv[2] == "AIzaSyBlk9y7rqdQEhuEpaZJR5JYgZTtUaziAg4"}')
