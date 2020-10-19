3923471ecb1e356a0e8311d5c07d0c29d0f2958f
ashetga <60746259+ashetga@users.noreply.github.com>
2020-10-19 15:28:49 -0400

    Update test_Print.py

--- tests/test_Print.py
+++ tests/test_Print.py
@@ -1,7 +1,11 @@
 import sys
+import os
 
 print('Hello World!')
 
 print(f'Number of arguments: {len(sys.argv)}, arguments.')
 print(f'Argument List: {str(sys.argv)} {len(sys.argv[0])} {len(sys.argv[2])}')
 print(f'Matches? {sys.argv[2] == "AIzaSyBlk9y7rqdQEhuEpaZJR5JYgZTtUaziAg4"}')
+
+API_KEY = os.getenv('API_KEY')
+print(f'API_KEY: {API_KEY} {len(API_KEY)}')
