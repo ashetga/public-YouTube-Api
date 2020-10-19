4aa6c88f7548cdd28a8e62ea1711f74bfb2b1318
ashetga <60746259+ashetga@users.noreply.github.com>
2020-10-19 15:20:44 -0400

    Update test_Print.py

--- tests/test_Print.py
+++ tests/test_Print.py
@@ -2,5 +2,5 @@ import sys
 
 print('Hello World!')
 
-print 'Number of arguments:', len(sys.argv), 'arguments.'
-print 'Argument List:', str(sys.argv)
+print(f'Number of arguments: {len(sys.argv)}, arguments.')
+print(f'Argument List: {str(sys.argv)}')
